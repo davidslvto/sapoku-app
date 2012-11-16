6dacad72f3714ce51b8486677a72b995d90cc198
Bruno de Carvalho <bruno@biasedbit.com>
2012-11-16 20:53:36 +0100

    Some shits

--- app/models/app.rb
+++ app/models/app.rb
@@ -3,6 +3,8 @@ class App < ActiveRecord::Base
 
   validates_uniqueness_of :name
 
+  after_destroy :delete_repo
+
   belongs_to :user
 
   def to_param
@@ -11,5 +13,11 @@ class App < ActiveRecord::Base
 
   def is_bootstrap_done?
     self.bootstraped == 2
+  end
+
+  def delete_repo
+    RepoHelper.execute REPO_PATH do |helper|
+      helper.delete_repo self.name
+    end
   end
 end
